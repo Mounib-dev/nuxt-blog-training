b043f0c3f7fb6b40965a2a755f54e3cce4f02003
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-08 14:12:30 +0100

    Initialization project commit -m

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Initialize project</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
