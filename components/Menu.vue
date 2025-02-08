f2410d6903e8685b27eed6948befc54efe8a5d1e
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-08 20:16:48 +0100

    Creating a menu component

---
+++ components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="font-mono">
+    <ul class="flex gap-5">
+      <li>
+        <NuxtLink to="/" class="link">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about" class="link">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/blog" class="link">Blog</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.link {
+  @apply p-1 hover:bg-gray-200;
+}
+</style>
