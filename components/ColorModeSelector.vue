aa77a21bf18ab4dcccf4e936155b396d190267b7
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-09 16:33:55 +0100

    Configuring and adding icons for toggle theme button

--- components/ColorModeSelector.vue
+++ components/ColorModeSelector.vue
@@ -1,17 +1,22 @@
 <template>
   <button
     @click="toggleTheme"
-    class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500 rounded-full"
+    class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500 rounded-full sticky top-0 float-right"
   >
-    {{ themeIcons[`${colorMode.preference === "light" ? "dark" : "light"}`] }}
+    <Icon
+      :name="
+        themeIcons[`${colorMode.preference === 'light' ? 'dark' : 'light'}`]
+      "
+      class="size-8 bg-gray-800 dark:bg-yellow-300"
+    />
   </button>
 </template>
 
 <script setup lang="js">
 const colorMode = useColorMode();
 const themeIcons = {
-  light: '🌕',
-  dark: '🌑'
+  light: 'ic:outline-wb-sunny',
+  dark: 'ic:outline-nightlight'
 }
 
 const toggleTheme = () => {
