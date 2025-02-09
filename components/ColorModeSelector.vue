8b6ea0b483bffbd2a73998c051c9299f1b0fd2d0
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-09 10:43:41 +0100

    Feat: toggle theme

---
+++ components/ColorModeSelector.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    @click="toggleTheme"
+    class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500 rounded-full"
+  >
+    {{ themeIcons[`${colorMode.preference === "light" ? "dark" : "light"}`] }}
+  </button>
+</template>
+
+<script setup lang="js">
+const colorMode = useColorMode();
+const themeIcons = {
+  light: '🌕',
+  dark: '🌑'
+}
+
+const toggleTheme = () => {
+    colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="container mx-auto max-w-3xl">
     <header class="flex justify-between items-center mt-5">
-      <div NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">
-        Mounib OUROUA
+      <div class="flex items-center space-x-12">
+        <div>
+          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">
+            Mounib OUROUA
+          </NuxtLink>
+        </div>
+        <Menu />
       </div>
-      <Menu />
+      <ColorModeSelector />
     </header>
-
     <main class="p-2 mt-10">
       <slot />
     </main>
@@ -16,7 +20,23 @@
 <script setup lang="ts">
 useHead({
   titleTemplate: "%s - Mounib OUROUA",
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+      crossorigin: "",
+    },
+  ],
 });
 </script>
 
-<style scoped></style>
+<style>
+body {
+  font-family: "Roboto";
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300;
+}
+</style>
