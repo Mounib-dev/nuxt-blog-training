935a6225cdc6e948f58196fc63c85c64ba9bb784
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-09 10:42:09 +0100

    Displaying GitHub projects

--- components/ProjectsList.vue
+++ components/ProjectsList.vue
@@ -1,7 +1,36 @@
 <template>
-  <div></div>
+  <p class="mb-10">Take a look at my GitHub Projects</p>
+  <section v-if="error">Something went wrong... try again!</section>
+  <section v-else>
+    <ul class="grid grid-cols-1 gap-4">
+      <li
+        v-for="repository in data"
+        :key="repository.id"
+        class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+      >
+        <a :href="repository.html_url" target="_blank">
+          <div class="flex items-center justify-between">
+            <div class="font-semibold">{{ repository.name }}</div>
+            <div>{{ repository.id }}</div>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="js">
+const { error, data } = await useFetch(
+  "https://api.github.com/users/Mounib-dev/repos"
+);
+
+const repos = computed(() =>
+  data.value
+    .filter((repo) => repo.description)
+    .sort((a, b) => {
+      return a.description - b.description;
+    })
+);
+</script>
 
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,22 +5,7 @@
 <script setup lang="ts">
 useHead({
   title: "Home",
-  link: [
-    {
-      rel: "preconnect",
-      href: "https://fonts.googleapis.com",
-    },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
-      crossorigin: "",
-    },
-  ],
 });
 </script>
 
-<style>
-body {
-  font-family: "Roboto";
-}
-</style>
+<style></style>
